e805d0212615816745dffbbd1b533a52aa34fde2
benjiWeinstein <benji.weinstein@medcase.health>
2022-05-19 16:22:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import { Factory1 } from "./Components/Factory1";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+document.body.style.backgroundColor = '#f5f5f5';
+document.body.style.margin = '0';
+return <Factory1 />;
 }
-
-export default App;
